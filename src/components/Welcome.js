474f45835c04792826bb561ddbeb58a53f8e1d15
Yash708262 <yash708262@gmail.com>
2025-09-19 17:18:55 +0530

    sdfswf

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
-// write code for Welcome component here
+import React from "react";
+
+function Welcome({name}){
+    return(
+        <div>
+        <h1>Hey {name}!</h1>
+        <h2>Welcome to School.</h2>
+        <p>fwfwf</p>
+        </div>
+    )
+}
+export default Welcome
